534650174ac32b7d18e7f4684b45f90b4a3de6e0
sdffsdff <lshepeleva000@gmail.com>
2023-05-29 09:39:19 +0200

    First homework assignment. implemented methods:save, get, delete, clear, getAll, size in ArrayStorage class.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,58 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int index = -1;
 
     void clear() {
+        index = -1;
     }
 
     void save(Resume r) {
+        index++;
+        storage[index] = r;
+
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i <= index; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                resume = storage[i];
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int deletedIndex = -1;
+        for (int i = 0; i <= index; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                deletedIndex = i;
+                break;
+            }
+        }
+        if (deletedIndex != -1) {
+            for (int i = deletedIndex + 1; i <= index; i++) {
+                storage[i - 1] = storage[i];
+            }
+            index--;
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, index + 1);
     }
 
     int size() {
-        return 0;
+        return index + 1;
     }
 }
